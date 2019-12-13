388cf071176909c8b7c72e607e5809214dc29f28
Jorge Martin <jorgemartinlasaosa@gmail.com>
2019-12-13 19:01:50 +0100

    Version 1

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,6 @@
 package example
 
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.{SQLContext, SparkSession}
@@ -9,101 +10,95 @@ import org.apache.spark.sql.functions.expr
 
 import scala.io.Source
 
-object Hello {
+object Hello extends Serializable {
+    def main(args: Array[String]): Unit = {
 
+        val conf = new SparkConf().setAppName("predictor")
 
-  def main(args: Array[String]): Unit = {
+        val spark = SparkSession
+          .builder
+          .appName("predictor")
+          .config("spark.master", "local")
+          .master("local")
+          .getOrCreate()
 
-    // Each one can have its own path in the path.txt file, which should not be synced with git.
-    val textIterator : Iterator[String] = Source.fromResource("path").getLines()
-    val path = textIterator.next()
-    //val path = "D:/Dataset/2008.csv.bz2"
+        // Take a look to diverted and cancelled variables
+        val dataset = spark.read.format("csv").option("header", "true").load("D:/Dataset/2008.csv.bz2")
+        // var dataset = spark.read.format("csv").option("header","true").option("inferSchema","true").load(path)
 
+        // Dropping variables
+        val dropped_dataset = dataset
+          .limit(250)
+          // Removing the forbidden variables
+          .drop("ArrTime")
+          .drop("ActualElapsedTime")
+          .drop("AirTime")
+          .drop("TaxiIn")
+          .drop("Diverted")
+          .drop("CarrierDelay")
+          .drop("WeatherDelay")
+          .drop("NASDelay")
+          .drop("SecurityDelay")
+          .drop("LateAircraftDelay")
+          // Tail Number: is an identifier, so it is not going to be useful.
+          .drop("Year")
+          .drop("FlightNum")
+          .drop("TailNum")
+          .drop("Cancelled")
+          .drop("CancellationCode")
 
-    val conf = new SparkConf().setAppName("predictor")
+        // We transform DepTime, CRSDepTime and CRSArrTime to integer
+        val expressionDepTime = "(60*((DepTime - (DepTime%100))/100))+(DepTime%100)"
+        val expressionCRSDepTime = "(60*((CRSDepTime - (CRSDepTime%100))/100))+(CRSDepTime%100)"
+        val expressionCRSArrTime = "(60*((CRSArrTime - (CRSArrTime%100))/100))+(CRSArrTime%100)"
 
-    val spark = SparkSession
-      .builder
-      .appName("predictor")
-      .config("spark.master", "local")
-      .master("local")
-      .getOrCreate()
+        val typed_dataset = dropped_dataset
+          //Transformation of HHMM variables to integer minutes
+          .withColumn("DepTime", expr(expressionDepTime))
+          .withColumn("CRSDepTime", expr(expressionCRSDepTime))
+          .withColumn("CRSArrTime", expr(expressionCRSArrTime))
+          //Variables cast
+          .withColumn("Month", col("Month").cast(IntegerType))
+          .withColumn("DayofMonth", col("DayofMonth").cast(IntegerType))
+          .withColumn("DayOfWeek", col("DayOfWeek").cast(IntegerType))
+          .withColumn("DepTime", col("DepTime").cast(IntegerType))
+          .withColumn("CRSDepTime", col("CRSDepTime").cast(IntegerType))
+          .withColumn("CRSArrTime", col("CRSArrTime").cast(IntegerType))
+          .withColumn("CRSElapsedTime", col("CRSElapsedTime").cast(IntegerType))
+          .withColumn("ArrDelay", col("ArrDelay").cast(IntegerType))
+          .withColumn("DepDelay", col("DepDelay").cast(IntegerType))
+          .withColumn("Distance", col("Distance").cast(IntegerType))
+          .withColumn("TaxiOut", col("TaxiOut").cast(IntegerType))
 
+        val cols = typed_dataset.columns.
+          filter( _ != "ArrDelay").
+          filter( _ != "UniqueCarrier").
+          filter( _ != "Origin").
+          filter( _ != "Dest")
 
-    // Take a look to diverted and cancelled variables
-    val dataset = spark.read.format("csv").option("header", "true").load(path)
-    // var dataset = spark.read.format("csv").option("header","true").option("inferSchema","true").load(path)
+        val assembler = new VectorAssembler()
+          .setInputCols(cols)
+          .setOutputCol("features")
+          .setHandleInvalid("skip")
 
-    // Dropping variables
-    val dropped_dataset = dataset
-      .limit(250)
-      // Removing the forbidden variables
-      .drop("ArrTime")
-      .drop("ActualElapsedTime")
-      .drop("AirTime")
-      .drop("TaxiIn")
-      .drop("Diverted")
-      .drop("CarrierDelay")
-      .drop("WeatherDelay")
-      .drop("NASDelay")
-      .drop("SecurityDelay")
-      .drop("LateAircraftDelay")
-      // Tail Number: is an identifier, so it is not going to be useful.
-      .drop("Year")
-      .drop("FlightNum")
-      .drop("TailNum")
-      .drop("Cancelled")
-      .drop("CancellationCode")
+        val output = assembler.transform(typed_dataset)
+        output.show(truncate=false)
 
-    // We transform DepTime, CRSDepTime and CRSArrTime to integer
-    val expressionDepTime = "(60*((DepTime - (DepTime%100))/100))+(DepTime%100)"
-    val expressionCRSDepTime = "(60*((CRSDepTime - (CRSDepTime%100))/100))+(CRSDepTime%100)"
-    val expressionCRSArrTime = "(60*((CRSArrTime - (CRSArrTime%100))/100))+(CRSArrTime%100)"
+        val lr = new LinearRegression()
+          .setLabelCol("ArrDelay")
+          .setFeaturesCol("features")
+          .setMaxIter(10)
+          .setElasticNetParam(0.8)
 
-    val typed_dataset = dropped_dataset
-      //Transformation of HHMM variables to integer minutes
-      .withColumn("DepTime", expr(expressionDepTime))
-      .withColumn("CRSDepTime", expr(expressionCRSDepTime))
-      .withColumn("CRSArrTime", expr(expressionCRSArrTime))
-      //Variables cast
-      .withColumn("Month", col("Month").cast(IntegerType))
-      .withColumn("DayofMonth", col("DayofMonth").cast(IntegerType))
-      .withColumn("DayOfWeek", col("DayOfWeek").cast(IntegerType))
-      .withColumn("DepTime", col("DepTime").cast(IntegerType))
-      .withColumn("CRSDepTime", col("CRSDepTime").cast(IntegerType))
-      .withColumn("CRSArrTime", col("CRSArrTime").cast(IntegerType))
-      .withColumn("CRSElapsedTime", col("CRSElapsedTime").cast(IntegerType))
-      .withColumn("ArrDelay", col("ArrDelay").cast(IntegerType))
-      .withColumn("DepDelay", col("DepDelay").cast(IntegerType))
-      .withColumn("Distance", col("Distance").cast(IntegerType))
-      .withColumn("TaxiOut", col("TaxiOut").cast(IntegerType))
+        val lrmodel = lr.fit(output)
 
-    println("Rows number before duplication removal" + typed_dataset.count())
+        val trainingSummary = lrmodel.summary
+        println(s"numIterations: ${trainingSummary.totalIterations}")
+        println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
+        trainingSummary.residuals.show()
+        println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+        println(s"r2: ${trainingSummary.r2}")
 
-   // val cleaned_dataset = typed_dataset.dropDuplicates()
-
-    //println("Rows number before duplication removal" + cleaned_dataset.count())
-
-
-
-    //Linear Regression
-    //https://scalac.io/scala-spark-ml-machine-learning-introduction/#pipelines
-    val lr = new LinearRegression()
-      .setMaxIter(10)
-      .setRegParam(0.3)
-      .setElasticNetParam(0.8)
-      .setStandardization(true)
-      //.labelCol( "MV")
-      //.featuresCol//("features")
-
-
-    // Fit the model
-    val lrModel = lr.fit(typed_dataset)
-
-    // Print the coefficients and intercept for linear regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-    spark.stop()
-  }
+        spark.stop()
+    }
 }
-
